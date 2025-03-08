f5ac0493a1a50ba02b24bdf1809ccc3f69f89b58
Shuhaibh <shuhaib3913@gmail.com>
2025-03-08 14:15:22 +0530

    corrected a small error

--- Tcp_Server.c
+++ Tcp_Server.c
@@ -7,8 +7,8 @@
 
 #define MAX 80
 #define PORT 3913
-#define SA struct sockaddr
-#define SAI struct sockaddr_in
+#define SA sockaddr
+#define SAI sockaddr_in
 
 void chat(int connfd)
 {
@@ -22,9 +22,9 @@ void chat(int connfd)
         read(connfd, buff, sizeof(buff));
         printf("Client: %s", buff);
 
-        if (strncmp(buff, "Exit", 4) == 0)
+        if ((strncmp(buff, "exit", 4)) == 0)
         {
-            printf("Client Exited.Disconnecting Server!..\n");
+            printf("Client Exited.Disconnecting Server...\n");
             break;
         }
 
@@ -32,13 +32,14 @@ void chat(int connfd)
         bzero(buff, MAX);
 
         n = 0;
-        while ((buff[n++] = getchar()) == '\n')
+
+        while ((buff[n++] = getchar()) != '\n')
             ;
         write(connfd, buff, sizeof(buff));
 
-        if (strncmp(buff, "Exit", 4) == 0)
+        if ((strncmp(buff, "exit", 4)) == 0)
         {
-            printf("Server Exited!..\n");
+            printf("Server Exiting...\n");
             break;
         }
     }
@@ -49,18 +50,20 @@ int main()
     SAI client, server;
     int sockfd, connfd, len;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = sockect(ARPA_INET, SOCK_STREAM, 0);
     printf("Socket created successfully\n");
 
-    server.sin_family = AF_INET;
+    server.sin_family = ARPA_INET;
     server.sin_addr.s_addr = htonl(INADDR_ANY);
     server.sin_port = htons(PORT);
 
     if ((bind(sockfd, (SA *)&server, sizeof(server))) == 0)
-        printf("Socket binded successful\n");
+        printf("Binding successfull\n");
 
     if ((listen(sockfd, 5)) == 0)
-        printf("Server is listening...\n");
+        printf("Server is listening..\n");
+
+    len = sizeof(client);
 
     connfd = accept(sockfd, (SA *)&client, (socklen_t *)&len);
     printf("Server accepted the client");
