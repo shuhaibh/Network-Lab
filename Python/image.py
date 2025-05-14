c0d66b40e710cdf8b5b2c92230f775e6b2a0bd87
Shuhaibh <shuhaib3913@gmail.com>
2025-05-14 12:25:21 +0530

    images.py

--- Python/image.py
+++ Python/image.py
@@ -7,5 +7,6 @@ root = Tk()
 canvas = Canvas(root,width=300,height=300)
 canvas.pack()
 img=ImageTk.PhotoImage(Image.open("image.jpg"))
+canvas.image = img  # Keep a reference to avoid garbage collection
 canvas.create_image(20,20,anchor=NW,image=img)
 mainloop()
